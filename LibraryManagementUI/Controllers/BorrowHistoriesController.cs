6ca02bd5539a47d783fdd77f57b903a976669084
Abhinav Sharma <abhinav.sharma13@live.com>
2020-07-20 12:10:26 +1000

    Updating application

--- LibraryManagementUI/Controllers/BorrowHistoriesController.cs
+++ LibraryManagementUI/Controllers/BorrowHistoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryManagementUI.DAL;
+using LibraryManagementUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -26,20 +27,40 @@ namespace LibraryManagementUI.Controllers
         }
 
         // GET: BorrowHistories/Create
-        public ActionResult Create()
+        public ActionResult Create(int id)
         {
-            return View();
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+            var borrowHistory = new BorrowHistory
+            {
+                BookId = book.BookId,
+                BorrowDate = DateTime.Now
+            };
+            var customers = db.Customers.ToList();
+            ViewBag.CustomerId = new SelectList(customers, "CustomerId", "Name");
+            //ViewBag.customers = customers;
+            return View(borrowHistory);
         }
 
         // POST: BorrowHistories/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(BorrowHistory borrowHistory)
         {
             try
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.BorrowHistories.Add(borrowHistory);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(borrowHistory);
+                }
             }
             catch
             {
@@ -50,17 +71,33 @@ namespace LibraryManagementUI.Controllers
         // GET: BorrowHistories/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+            //where LINQ
+            var borrowHistory = db.BorrowHistories
+                                    .Include(b => b.book)
+                                    .Include(b => b.customer)
+                                    .Where(b => b.BookId == id && b.ReturnDate == null)
+                                        .FirstOrDefault();
+            if (borrowHistory == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            return View(borrowHistory);
         }
 
         // POST: BorrowHistories/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(BorrowHistory borrowHistory)
         {
             try
             {
-                // TODO: Add update logic here
-
+                borrowHistory.ReturnDate = DateTime.Now;
+                db.Entry(borrowHistory).State = EntityState.Modified;
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
