3707f2df27e39ed7c95889d746df1af4b3cd3419
Abhinav Sharma <abhinav.sharma13@live.com>
2020-07-22 12:25:19 +1000

    Adding DataAccessLayer

---
+++ DataAccessLayer/IDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IDAL
+    {
+        DataTable ExecuteQuery(string sqlQuery);
+
+        DataTable ExecuteStoredProcedure(string procedureName);
+
+        DataTable ExecuteStoredProcedure<T>(string procedureName, List<T> parameters) where T : DbParameter;
+    }
+}
---
+++ DataAccessLayer/MySQLDALManager.cs
@@ -0,0 +1,81 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class MySQLDALManager : IDAL
+    {
+        MySqlConnection connection;
+
+        public MySQLDALManager(string dbConnKey)
+        {
+            String connString = ConfigurationManager.ConnectionStrings[dbConnKey].ConnectionString;
+            connection = new MySqlConnection(connString);
+        }
+
+        public DataTable ExecuteQuery(string sqlQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable ExecuteStoredProcedure(string procedureName)
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand(procedureName, connection);
+            command.CommandType = CommandType.StoredProcedure;
+            adapter.SelectCommand = command;
+            DataTable data = new DataTable();
+            try
+            {
+                connection.Open();
+                adapter.Fill(data);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                //throw ex;
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public DataTable ExecuteStoredProcedure<T>(string procedureName, List<T> parameters) where T : DbParameter
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand(procedureName, connection);
+            command.CommandType = CommandType.StoredProcedure;
+            foreach (DbParameter param in parameters)
+            {
+                command.Parameters.Add(param);
+            }
+            adapter.SelectCommand = command;
+            DataTable data = new DataTable();
+            try
+            {
+                connection.Open();
+                adapter.Fill(data);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
